a136a56e08016d09ac87b7f5d9fbba6cd015cf5c
Mark Tolmacs <mark@lazycat.hu>
2023-08-15 17:33:41 +0200

    Tool from emscripten wasm sourcemap generation added

--- example/install.sh
+++ example/install.sh
@@ -6,4 +6,12 @@ if command -v npm &> /dev/null
 then
     cd ../tools/decode
     npm i
+    cd ../tools/source-map
+    npm i
+fi
+if command -v python3 &> /dev/null
+then
+    python3 -m pip install --user virtualenv
+    cd ../tools/wasm-sourcemap
+    python3 -m venv .env
 fi
