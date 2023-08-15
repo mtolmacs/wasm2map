7884dfce1d32c39f0772f07dbf81c9baee10491d
Mark Tolmacs <mark@lazycat.hu>
2023-08-15 17:01:17 +0200

    UNIX example run and install fixes

--- example/install.sh
+++ example/install.sh
@@ -2,3 +2,8 @@
 
 rustup target add wasm32-unknown-unknown
 cargo install devserver
+if command -v npm &> /dev/null
+then
+    cd ../tools/decode
+    npm i
+fi
--- example/run.sh
+++ example/run.sh
@@ -2,12 +2,12 @@
 
 cargo clean
 cargo build --target wasm32-unknown-unknown
-COPY src/index.html target/wasm32-unknown-unknown/debug/
+cp src/index.html target/wasm32-unknown-unknown/debug/
 touch target/wasm32-unknown-unknown/debug/favicon.ico
 cargo install --path ../cargo-wasm2map
 cargo wasm2map --bundle-sources -p -b http://127.0.0.1:8080 target/wasm32-unknown-unknown/debug/example.wasm
-if command -v <the_command> &> /dev/null
+if command -v node &> /dev/null
 then
-    node ..\tools\decode\decode.js .\target\wasm32-unknown-unknown\debug\example.wasm.map .\target\wasm32-unknown-unknown\debug\sourcemap.json || true
+    node ../tools/decode/decode.js ./target/wasm32-unknown-unknown/debug/example.wasm.map ./target/wasm32-unknown-unknown/debug/sourcemap.json || true
 fi
 devserver --noreload --path target/wasm32-unknown-unknown/debug/ --address 127.0.0.1:8080
