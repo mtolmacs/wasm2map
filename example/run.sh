c27ce3c19f2cac4b4361776b602a76bc1f250e6a
Mark Tolmacs <mark@lazycat.hu>
2023-08-22 16:12:31 +0200

    Unix example run fix

--- example/run.sh
+++ example/run.sh
@@ -5,11 +5,9 @@ cargo build --target wasm32-unknown-unknown
 cp src/index.html target/wasm32-unknown-unknown/debug/
 cp src/index.ts target/wasm32-unknown-unknown/debug/
 cp src/util.ts target/wasm32-unknown-unknown/debug/
-tsc --sourcemap target/wasm32-unknown-unknown/debug/index.ts
-tsc --sourcemap target/wasm32-unknown-unknown/debug/util.ts
 touch target/wasm32-unknown-unknown/debug/favicon.ico
 cargo install --path ../cargo-wasm2map
-cargo wasm2map --bundle-sources -p -b http://127.0.0.1:8080 target/wasm32-unknown-unknown/debug/example.wasm
+cargo wasm2map --bundle-sources -p -b http://192.168.1.111:8080 target/wasm32-unknown-unknown/debug/example.wasm
 node ../tools/decode/decode.js ./target/wasm32-unknown-unknown/debug/example.wasm.map ./target/wasm32-unknown-unknown/debug/sourcemap.json || true
 node_modules/.bin/tsc --sourcemap -m es6 target/wasm32-unknown-unknown/debug/index.ts
 node_modules/.bin/tsc --sourcemap -m es6 target/wasm32-unknown-unknown/debug/util.ts
