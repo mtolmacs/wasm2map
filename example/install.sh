cfad9db943d197fbdbc0c4b33d74ea5d11b4bf9d
Mark Tolmacs <mark@lazycat.hu>
2023-08-22 14:49:48 +0200

    Example launch Unix updates

--- example/install.sh
+++ example/install.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env sh
 
 rustup target add wasm32-unknown-unknown
-cargo install devserver
-if command -v npm &> /dev/null
-then
-    cd ../tools/decode
-    npm i
-    cd ../tools/source-map
-    npm i
-fi
+cargo install simple-http-server
+npm i typescript
+cd ../tools/decode
+npm i
+cd ../source-map
+npm i
 if command -v python3 &> /dev/null
 then
     python3 -m pip install --user virtualenv
-    cd ../tools/wasm-sourcemap
+    cd ../wasm-sourcemap
     python3 -m venv .env
 fi
--- example/run.sh
+++ example/run.sh
@@ -3,11 +3,12 @@
 cargo clean
 cargo build --target wasm32-unknown-unknown
 cp src/index.html target/wasm32-unknown-unknown/debug/
+cp src/index.ts target/wasm32-unknown-unknown/debug/
+cp src/util.ts target/wasm32-unknown-unknown/debug/
 touch target/wasm32-unknown-unknown/debug/favicon.ico
 cargo install --path ../cargo-wasm2map
 cargo wasm2map --bundle-sources -p -b http://127.0.0.1:8080 target/wasm32-unknown-unknown/debug/example.wasm
-if command -v node &> /dev/null
-then
-    node ../tools/decode/decode.js ./target/wasm32-unknown-unknown/debug/example.wasm.map ./target/wasm32-unknown-unknown/debug/sourcemap.json || true
-fi
-devserver --noreload --path target/wasm32-unknown-unknown/debug/ --address 127.0.0.1:8080
+node ../tools/decode/decode.js ./target/wasm32-unknown-unknown/debug/example.wasm.map ./target/wasm32-unknown-unknown/debug/sourcemap.json || true
+node_modules/.bin/tsc --sourcemap -m es6 target/wasm32-unknown-unknown/debug/index.ts
+node_modules/.bin/tsc --sourcemap -m es6 target/wasm32-unknown-unknown/debug/util.ts
+simple-http-server -p 8080 -i target/wasm32-unknown-unknown/debug/
