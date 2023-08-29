2c3852a8e91d02b78ab674e29cb6b7d531d78299
Mark Tolmacs <mark@lazycat.hu>
2023-08-29 19:09:59 +0200

    Stripping DWARF is important to not mess up Firefox

--- example/run.sh
+++ example/run.sh
@@ -11,5 +11,5 @@ cargo wasm2map --bundle-sources -p -b http://192.168.1.111:8080 target/wasm32-un
 node ../tools/decode/decode.js ./target/wasm32-unknown-unknown/debug/example.wasm.map ./target/wasm32-unknown-unknown/debug/sourcemap.json || true
 node_modules/.bin/tsc --sourcemap -m es6 target/wasm32-unknown-unknown/debug/index.ts
 node_modules/.bin/tsc --sourcemap -m es6 target/wasm32-unknown-unknown/debug/util.ts
-#wasm-opt -Os --strip-dwarf -o target/wasm32-unknown-unknown/debug/example.wasm target/wasm32-unknown-unknown/debug/example.wasm
+wasm-opt -Os --strip-dwarf -o target/wasm32-unknown-unknown/debug/example.wasm target/wasm32-unknown-unknown/debug/example.wasm
 simple-http-server -p 8080 -i target/wasm32-unknown-unknown/debug/
