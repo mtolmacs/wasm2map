6d8ebbe57f9c162bcd06ddee2806a5c5fb6df8b7
Márk Tolmács <mark@lazycat.hu>
2023-08-31 19:04:37 +0200

    Small example testing script fixes

--- example/install.bat
+++ example/install.bat
@@ -1,3 +1,8 @@
 @ECHO OFF
 rustup target add wasm32-unknown-unknown
 cargo install devserver
+npm i typescript
+cd ../tools/decode
+npm i
+cd ../source-map
+npm i
--- example/run.bat
+++ example/run.bat
@@ -6,7 +6,7 @@ COPY src\index.ts target\wasm32-unknown-unknown\debug\
 COPY src\util.ts target\wasm32-unknown-unknown\debug\
 copy nul >> target\wasm32-unknown-unknown\debug\favicon.ico
 cargo install --path ..\cargo-wasm2map
-cargo wasm2map -p -b http://127.0.0.1:8080 --bundle-sources target\wasm32-unknown-unknown\debug\example.wasm
+cargo wasm2map -p -b http://localhost:8080 --bundle-sources target\wasm32-unknown-unknown\debug\example.wasm
 REM wasm-opt -Os --strip-dwarf -o target\wasm32-unknown-unknown\debug\example.wasm target\wasm32-unknown-unknown\debug\example.wasm
 node ..\tools\decode\decode.js .\target\wasm32-unknown-unknown\debug\example.wasm.map .\target\wasm32-unknown-unknown\debug\sourcemap.json
 cmd.exe /c tsc --sourcemap -m es6 target\wasm32-unknown-unknown\debug\index.ts 
