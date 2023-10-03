34eb3e5b4f75854510f8d1597d12f0764474dfbf
Mark Tolamcs <mark@lazycat.hu>
2023-10-03 15:44:00 +0200

    example install windows python

--- example/install.bat
+++ example/install.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 rustup target add wasm32-unknown-unknown
 cargo install simple-http-server
-npm i typescript && cd ../tools/decode && npm i && cd ../source-map && npm i
+npm i typescript && cd ../tools/decode && npm i && cd ../source-map && npm i && cd ../wasm-sourcemap && python3 -m venv .env && .\.env\Scripts\pip3.exe install -r requirements.txt && cd ../../example
